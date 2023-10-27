61095f145868200b515595683d7120b28dfd1c16
Bananeq <dankilom26@gmail.com>
2023-10-27 17:08:28 +0200

    create gradle project

---
+++ cs_skin_system/src/main/kotlin/cz/cvut/fit/tjv/cs_skin_system/CsSkinSystemApplication.kt
@@ -0,0 +1,11 @@
+package cz.cvut.fit.tjv.cs_skin_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsSkinSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsSkinSystemApplication>(*args)
+}
---
+++ cs_skin_system/src/test/kotlin/cz/cvut/fit/tjv/cs_skin_system/CsSkinSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.cs_skin_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsSkinSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
