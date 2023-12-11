01f1030fa239ab6067d108ac1d6da71bf22c8d15
Bananeq <dankilom26@gmail.com>
2023-12-11 22:31:09 +0100

    Added JpaSpecificationExecutor inheritance to SkinRepository

--- cs_skin_system/src/main/kotlin/cz/cvut/fit/tjv/cs_skin_system/persistent/JPASkinRepository.kt
+++ cs_skin_system/src/main/kotlin/cz/cvut/fit/tjv/cs_skin_system/persistent/JPASkinRepository.kt
@@ -2,12 +2,13 @@ package cz.cvut.fit.tjv.cs_skin_system.persistent
 
 import cz.cvut.fit.tjv.cs_skin_system.domain.Skin
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface JPASkinRepository : JpaRepository<Skin, Long> {
+interface JPASkinRepository : JpaRepository<Skin, Long>, JpaSpecificationExecutor<Skin> {
     fun existsByNameAndPaintSeedAndFloatAndWeaponId(name: String, paintSeed: Int, float: Double, weaponId: Long): Boolean
 
     @Query("SELECT s FROM Skin s JOIN s.dropsFrom c WHERE s.rarity = :rarity AND s.price > :price AND c.name = :caseName")
