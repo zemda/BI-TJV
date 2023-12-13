265a5b3aec97eeb99d8abc769901813b4ea46126
Bananeq <dankilom26@gmail.com>
2023-12-13 21:28:17 +0100

    CsgoCaseController initial implementation
    - m:n relationship isnt correctly handled yet

---
+++ cs_skin_system/src/main/kotlin/cz/cvut/fit/tjv/cs_skin_system/controller/CsgoCaseController.kt
@@ -0,0 +1,93 @@
+package cz.cvut.fit.tjv.cs_skin_system.controller
+
+import cz.cvut.fit.tjv.cs_skin_system.application.CsgoCaseService
+import cz.cvut.fit.tjv.cs_skin_system.domain.CsgoCase
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/csgoCase")
+@CrossOrigin(origins = ["http://localhost:5173"])
+class CsgoCaseController(private val csgoCaseService: CsgoCaseService) {
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a CSGO case by its id")
+    fun getCsgoCaseById(@PathVariable id: Long): ResponseEntity<Any> {
+        return try {
+            val csgoCase = csgoCaseService.getCsgoCaseById(id)
+            ResponseEntity(csgoCase, HttpStatus.OK)
+        } catch (e: NoSuchElementException) {
+            ResponseEntity(e.message ?: "Case not found", HttpStatus.NOT_FOUND)
+        } catch (e: Exception) {
+            ResponseEntity(e.message ?: "Server error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GetMapping
+    fun getCsgoCases(): ResponseEntity<List<CsgoCase>> {
+        return ResponseEntity.ok(csgoCaseService.getCsgoCases())
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a CSGO case")
+    fun createCsgoCase(@RequestBody csgoCase: CsgoCase): ResponseEntity<Any> {
+        return try {
+            val createdCsgoCase = csgoCaseService.createCsgoCase(csgoCase)
+            ResponseEntity(createdCsgoCase, HttpStatus.CREATED)
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity(e.message ?: "Case name already exists", HttpStatus.CONFLICT)
+        } catch (e: Exception) {
+            ResponseEntity(e.message ?: "Server error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @PutMapping("/{caseId}/changePrice/{newPrice}")
+    @Operation(summary = "Change price of a CSGO case")
+    fun changePrice(@PathVariable caseId: Long, @PathVariable newPrice: Double): ResponseEntity<Any> {
+        return try {
+            val updatedCsgoCase = csgoCaseService.updateCsgoCase(caseId, newPrice)
+            ResponseEntity(updatedCsgoCase, HttpStatus.OK)
+        } catch (e: NoSuchElementException) {
+            ResponseEntity(e.message ?: "CSGO Case not found", HttpStatus.NOT_FOUND)
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity(e.message ?: "Invalid new price", HttpStatus.BAD_REQUEST)
+        } catch (e: Exception) {
+            ResponseEntity(e.message ?: "Server error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @PutMapping("/{caseId}/addSkins")
+    @Operation(summary = "Add skins to a CSGO case")
+    fun addSkinsToCase(@PathVariable caseId: Long, @RequestBody skinIds: List<Long>,
+                       @RequestParam addSkins: Boolean): ResponseEntity<Any> {
+        return try {
+            val updatedCsgoCase = csgoCaseService.updateCsgoCase(caseId, skinIds, addSkins)
+            ResponseEntity(updatedCsgoCase, HttpStatus.OK)
+        } catch (e: NoSuchElementException) {
+            when {
+                e.message?.contains("case") ?: false -> ResponseEntity(e.message ?: "CSGO Case not found", HttpStatus.NOT_FOUND)
+                e.message?.contains("skin") ?: false -> ResponseEntity(e.message ?: "Skin not found", HttpStatus.NOT_FOUND)
+                else -> ResponseEntity(e.message ?: "Item not found", HttpStatus.NOT_FOUND)
+            }
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity(e.message ?: "Illegal state detected", HttpStatus.CONFLICT)
+        } catch (e: Exception) {
+            ResponseEntity(e.message ?: "Server error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Delete a CSGO case")
+    fun deleteCsgoCase(@RequestBody csgoCase: CsgoCase): ResponseEntity<Any> {
+        return try {
+            csgoCaseService.deleteCsgoCase(csgoCase)
+            ResponseEntity(HttpStatus.NO_CONTENT)
+        } catch (e: NoSuchElementException) {
+            ResponseEntity(e.message ?: "CSGO Case not found", HttpStatus.NOT_FOUND)
+        } catch (e: Exception) {
+            ResponseEntity(e.message ?: "Server error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
