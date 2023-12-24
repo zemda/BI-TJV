3921766f8201a0de1d62cfe56c685c8f23dabbd5
Bananeq <dankilom26@gmail.com>
2023-12-24 23:25:42 +0100

    fix SkinControllerTest

--- cs_skin_system/src/test/kotlin/cz/cvut/fit/tjv/cs_skin_system/controller/SkinControllerTest.kt
+++ cs_skin_system/src/test/kotlin/cz/cvut/fit/tjv/cs_skin_system/controller/SkinControllerTest.kt
@@ -2,51 +2,44 @@ package cz.cvut.fit.tjv.cs_skin_system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import cz.cvut.fit.tjv.cs_skin_system.application.SkinService
-import cz.cvut.fit.tjv.cs_skin_system.domain.Skin
+import cz.cvut.fit.tjv.cs_skin_system.dto.SkinCreateDTO
 import cz.cvut.fit.tjv.cs_skin_system.dto.SkinDTO
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito.doNothing
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@ExtendWith(SpringExtension::class)
 @WebMvcTest(SkinController::class)
 class SkinControllerTest {
 
     @Autowired lateinit var mockMvc: MockMvc
-
     @Autowired lateinit var objectMapper: ObjectMapper
-
     @MockBean lateinit var skinService: SkinService
 
     @Test
-    fun `createSkin create new skin and return status CREATED`() {
-        val skin =
-                Skin().apply {
-                    name = "Test Skin"
-                    rarity = "Rare"
-                    float = 0.1
-                    paintSeed = 10
-                }
+    fun whenCreateSkinCalled_thenReturnStatusCreated() {
+        val skin = SkinCreateDTO("Test Skin", "Rare", 0.1, 10, 0.002, null, null)
+        val skinDto = SkinDTO(1L, "Test Skin", "Rare", "Factory New", 0.1, 10, 0.002, null)
 
+        `when`(skinService.create(skin)).thenReturn(skinDto)
         mockMvc.perform(
                         post("/skins")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(skin))
                 )
                 .andExpect(status().isCreated)
+                .andExpect(content().json(objectMapper.writeValueAsString(skinDto)))
+        verify(skinService, times(1)).create(skin)
     }
 
     @Test
-    fun `getSkinById return skin if it exists`() {
+    fun whenGetSkinByIdCalled_thenReturnSkinIfExists() {
         val id = 1L
         val skinDto = SkinDTO(id, "Test Skin", "Rare", "idk", 0.001, 104, 0.001, null)
 
@@ -54,15 +47,19 @@ class SkinControllerTest {
 
         mockMvc.perform(get("/skins/$id").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk)
+                .andExpect(content().json(objectMapper.writeValueAsString(skinDto)))
+        verify(skinService, times(1)).getById(id)
     }
 
     @Test
-    fun `deleteSkin delete a skin if it exists`() {
+    fun whenDeleteSkinCalled_thenReturnStatusNoContent() {
         val id = 1L
 
         doNothing().`when`(skinService).deleteById(id)
 
         mockMvc.perform(delete("/skins/$id").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent)
+
+        verify(skinService, times(1)).deleteById(id)
     }
 }
